806e92b20dc5ab3f61e27d8014b614a1dcf43023
Samiksha Awate <samikshaawate7@gmail.com>
2026-03-01 16:04:08 +0530

    Add More Products

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -84,7 +84,7 @@ products.forEach((product) =>{
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="rating-${product.rating.stars * 10}.png">
+              src="images/ratings/rating-${product.rating.stars * 10}.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
