8fc9f4e55ad5dfa0ff59429908304e1051e48783
Samiksha Awate <samikshaawate7@gmail.com>
2026-03-02 15:36:20 +0530

    Add Product File

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,73 +1,3 @@
-const products = [{
-
-    image :"images/products/athletic-cotton-socks-6-pairs.jpg",
-    name : ' Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating : {
-        stars :4.5,
-        count : 87
-    },
-    priceCents: 1090
-
-},{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name :' Intermediate Size Basketball',
-    rating:{
-        stars :4.0,
-        count : 127
-    },
-    priceCents:2095
-},
-{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name :' Adults Plain Cotton T-Shirt - 2 Pack',
-    rating :{
-        stars :4.5,
-        count:56
-    },
-    priceCents:799
-},{
-    image: 'images/products/black-2-slot-toaster.jpg',
-    name :'2 Slot Toaster-Black',
-    rating :{
-        stars:5,
-        count:2197
-    },
-    priceCents:1899
-},
-{
-    image: 'images/products/6-piece-white-dinner-plate-set.jpg',
-    name :'6 Piece White Dinner Plate Set',
-    rating :{
-        stars:4,
-        count:37
-    },
-    priceCents:2067
-},{
-    image: 'images/products/plain-hooded-fleece-sweatshirt-yellow.jpg',
-    name :'Plain Hooded Fleece Sweatshirt',
-    rating :{
-        stars:4.5,
-        count:317
-    },
-    priceCents:2400
-},
-{
-    image: 'images/products/cotton-bath-towels-teal.webp',
-    name :'100% Cotton Bath Towels - 2 Pack, Light Teal',
-    rating :{
-        stars:4.5,
-        count:93
-    },
-    priceCents:2110
-},{
-   image: 'images/products/men-navigator-sunglasses-brown.jpg',
-    name :"Men's Navigator Sunglasses Pilot",
-    rating :{
-        stars:3.5,
-        count:42
-    },
-    priceCents:1690
-}];
 
 let productHTML ='';
 products.forEach((product) =>{
@@ -117,7 +47,7 @@ products.forEach((product) =>{
           </div>
 
           <button class="add-to-cart-button button-primary js-add-to-cart"
-          data-product-name="${product.name}">
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -132,11 +62,11 @@ innerHTML = productHTML;
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
 button.addEventListener('click', () =>{
-  const productName = button.dataset.productName;
-
+  const productId = button.dataset.productId;
+ 
   let matchingItem ;
   cart.forEach((item) => {
-    if(productName === item.productName){
+    if(productId === item.productId){
       matchingItem = item;
     }
   });
@@ -145,11 +75,11 @@ button.addEventListener('click', () =>{
     matchingItem.quality += 1;
   }else{
     cart.push({
-    productName:productName,
+    productId:productId,
     quality :1
   });
   }
-
+console.log(cart);
 
 });
 });
