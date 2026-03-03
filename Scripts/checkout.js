d18db73cd6b9b85e3f9badf2e23d49c3eada486d
Samiksha Awate <samikshaawate7@gmail.com>
2026-03-03 21:47:07 +0530

    Work on Delivery Option

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,3 +1,7 @@
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMM D'));
+
 let cartSummaryHTML = '';
 cart.forEach((cartItem) =>{
 
@@ -45,50 +49,48 @@ cart.forEach((cartItem) =>{
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio"
-                    class="delivery-option-input"
-                    name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                
+                
+                ${deliveryOptionsHTML(matchingProduct)}
+                
               </div>
             </div>
           </div>
     `;
 });
+
+function deliveryOptionsHTML(matchingProduct){
+  let html = '';
+    deliveryOptions.forEach((deliveryOption) => {
+      const today = dayjs();
+      const deliveryDate = today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      );
+      const dateString = deliveryDate.format(
+        'dddd, MMMM D'
+      );
+
+      const priceString = deliveryOption.priceCents === 0 
+      ? 'FREE'
+      : `$${(deliveryOption.priceCents / 100).toFixed(2)} -`;
+
+    html +=`<div class="delivery-option">
+              <input type="radio"
+                class="delivery-option-input"
+                name="delivery-option-${matchingProduct.id}">
+              <div>
+                <div class="delivery-option-date">
+                  ${dateString}
+                </div>
+                <div class="delivery-option-price">
+                  ${priceString}- Shipping
+                </div>
+              </div>
+            </div>`;
+    });
+    return html;
+}
 
 document.querySelector('.js-order-summary')
 .innerHTML = cartSummaryHTML;
