3ed6a51e8f3b6fc3b419fab711c91bfd431fcbc9
Samiksha Awate <samikshaawate7@gmail.com>
2026-03-03 09:34:07 +0530

    Generate HTML for the Order Summary

--- Scripts/checkout.js
+++ Scripts/checkout.js
@@ -1,3 +1,4 @@
+let cartSummaryHTML = '';
 cart.forEach((cartItem) =>{
 
     const productId = cartItem.productId;
@@ -8,7 +9,7 @@ cart.forEach((cartItem) =>{
         }
     });
 
-    console.log(matchingProduct);
+    cartSummaryHTML += 
     `
     <div class="cart-item-container">
             <div class="delivery-date">
@@ -17,18 +18,18 @@ cart.forEach((cartItem) =>{
 
             <div class="cart-item-details-grid">
               <img class="product-image"
-                src="images/products/athletic-cotton-socks-6-pairs.jpg">
+                src="${matchingProduct.image}">
 
               <div class="cart-item-details">
                 <div class="product-name">
-                  Black and Gray Athletic Cotton Socks - 6 Pairs
+                  ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $10.90
+                  $${matchingProduct.priceCents / 100}
                 </div>
                 <div class="product-quantity">
                   <span>
-                    Quantity: <span class="quantity-label">2</span>
+                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
                   </span>
                   <span class="update-quantity-link link-primary">
                     Update
@@ -85,5 +86,8 @@ cart.forEach((cartItem) =>{
               </div>
             </div>
           </div>
-    `
+    `;
 });
+
+document.querySelector('.js-order-summary')
+.innerHTML = cartSummaryHTML;
